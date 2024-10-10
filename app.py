db59942e87bf6bd7db21d0c118cd56400cf408fc
jmorales01 <joseluismoralesvega24@gmail.com>
2024-10-09 22:37:33 -0500

    feat: configurar cors globales

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://skincheck-app.vercel.app/",],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
